e159d38a64b267034f5132bbc75f834adf301b11
ARG20091 <82979447+ARG20091@users.noreply.github.com>
2021-08-08 09:28:48 -0500

    Add files via upload

---
+++ synchronousBallMovement-master/synchronousBallMovement-master/sketch.js
@@ -0,0 +1,16 @@
+var hypnoticBall, database;
+var position;
+var form, player, game, playerCount
+var gameState = 0
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(400,400);
+  game = new Game()
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  background("white");
+}
